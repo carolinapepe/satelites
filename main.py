5d73bc702c33edb5b5c67162ccdb3ac8aadf4f55
carolinapepe <carolina.pepe@gmail.com>
2016-03-15 17:39:25 -0300

    Agregamos subclase(ascat) de satelite con su metodo que empieza a armar la url. Llevamos la fecha al formato adecuado. Agregamos un argumento de entrada: los satelites que quiero excluir del plot

--- main.py
+++ main.py
@@ -2,22 +2,50 @@
 
 import argparse 
 import satellite
+import DateTime
+
+def construct_site_list(not_in_list):
+
+    all_sites = ['ASCAT', 'OTROSAT','OTROSAT2']
+    sites = list(set(all_sites) - set(not_in_list))
+    return sites
+
+def plot_data_matrix(data_matrix):
+    pass
+
 
 def main():
   
 	#define parser data
-  
+    #ACLARAR BIEN QUe ESTAMOS SUBIENDO Y EN QUE FORMATO
     parser = argparse.ArgumentParser(description='Plotting satellite data.')
-    parser.add_argument('date', metavar='date_time', type=int, nargs=2,\
+    parser.add_argument('date', metavar='date_time_YYYY.MM.DD', type=str, nargs=2,\
 		      help='time interval')
-    parser.add_argument('satellite', metavar='SATELLITE_NAME', type=str, nargs=1,\
+    parser.add_argument('satellite', metavar='SATELLITE_NAME', type=str, nargs='?',\
 		      help='satellite name')
 
     args=parser.parse_args()
     initialDate = args.date[0]
     finalDate = args.date[1]
+
+    if not args.satellite:
+        not_in_list = []
+    else:
+        not_in_list = args.satellite.split('+')
     
-    a_satellite = satellite.Satellite(args.satellite[0],initialDate, finalDate)
+    sites = construct_site_list(not_in_list)
+    
+    ascat = satellite.ASCAT(initialDate, finalDate)
+    ascat.convert_to_day_of_the_year()
+    print ascat.initial_time,ascat.final_time
+    #print ascat.initial_time, ascat.final_time
+    
+    for site in sites:
+        pass
+        #a_satellite = satellite.Satellite(args.satellite[0],initialDate, finalDate)
+    
+    
 
 if __name__ == "__main__":
     main()  
+       
--- satellite.py
+++ satellite.py
@@ -1,18 +1,46 @@
+import DateTime
+
 class Satellite(object):
 
-    def __init__(self, name, initial_time, final_time):
+    def __init__(self, initial_time, final_time):
         self.initial_time = initial_time
         self.final_time = final_time
-        self.name = name
-        self.url = self.choose_url()
+        
+        
 	  
-    def choose_url(self):
-        print self.name
-        if self.name == 'ASCAT':
-            self.url = 'poner url'
-            print self.url
-        elif self.name == 'OTRO':
-            self.url = 'PONER OTRO'
 
   
+class ASCAT(Satellite):
+    def __init__(self,initial_time, final_time):
+        Satellite.__init__(self,initial_time, final_time)
+        #self.convert_to_julian_date(self)
+        
+    def convert_to_day_of_the_year(self):
+        formt = '%Y.%m.%d'
+        self.initial_time = DateTime.DateTime(self.initial_time).dayOfYear()
+        self.final_time = DateTime.DateTime(self.final_time).dayOfYear()
+        
+        
+    def make_URLs(self):
+        pass
+        #root_URL = 'ftp://podaac.jpl.nasa.gov/allData/ascat/preview/L2/metop_a/25km/'
+        #url
+        
+    def download_data(self):
+        pass
+    def make_data_matrix(self):
+        pass
+
+"""    
+class OTROSAT(object):
     
+    def __init__(self...):
+        pass
+    def make_URL(self,initial_date,final_date...):
+        
+        
+        pass
+    def make_data_matrix(self,...):
+        pass
+    
+"""
