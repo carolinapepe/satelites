50ade39a265188a9c0d88e03a8d0a3cb3e3a781d
spanish inquisition <wtpc@gorgona>
2016-03-17 11:55:18 -0400

    Documentacion. modifique README y agregue overview

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # satelites
+
+Este directorio contiene un programa que plotea mediciones de velocidad de viento realizadas por satelites en un intervalo de fechas asignado por el usuario.
+
+
+## Producto ploteado
+
+A la fecha el programa esta implementado unicamente para productos obtenidos del ASCAT (Advanced Scatterometer) perteneciente al MetOp-A (Meteorological Operational satellite program). Perteneciente a EUMETSAT (European Organization for the Exploitation of Meteorological Satellites).
+
+El producto extraido para el ploteo es velocidad del viento sobre superficie oceanica con una resolucion de muestreo de 25 km. El intervalo de tiempo maximo de datos disponibles es [2012-2016].
+
+El set de datos se descarga de directorio 
+http://podaac-opendap.jpl.nasa.gov/opendap/allData/ascat/preview/L2/metop_b/25km/
+MARKDOWN
+
+En overview.md se describen las librerias implemetadas en el programa principal y los modulos.
+
+
+
+
+
+
+
+
+
+
--- read_data.py
+++ read_data.py
@@ -1,17 +1,34 @@
 
-""" Lee los datos descargados"""
 
-import netCDF4 as nc
-from netCDF4 import Dataset
+#Busca archivos del servidos ftp y guarda en directorio source
+#MAURO
 
-#Creo objeto netCDF a partir del archivo descargado
-f=Dataset("./Downloads/ascat_20160101_003000_metopa_47740_eps_o_250_2300_ovw.l2.nc","r")
+#Descomprime archivos .gz en el directorio source
+""" Descomprime archivos .gz en source"""
 
-#Extraigo las variables de locacion y tiempo
-lat=f.variables['lat'][:]
-lon=f.variables['lon'][:]
-time=f.variables['time'][:]
+import gzip
+import glob
+import os.path
+import shutil
 
-#Extraigo variable a plotear
-wspeed=f.variables['wind_speed'][:]
+source_dir = "/tmp/source"
+dest_dir = "/tmp/dest"
+tmpfile = "/tmp/delete.me"
+
+
+for src_name in glob.glob(os.path.join(source_dir, '*.gz')):
+    base = os.path.basename(src_name)
+    dest_name = os.path.join(dest_dir, base[:-3])
+    print dest_name
+    shutil.copyfile(src_name, tmpfile)
+    with gzip.open(tmpfile, 'rb') as infile:
+        with open(dest_name, 'w') as outfile:
+            for line in infile:
+                outfile.write(line)
+
+
+
+
+
+
 
